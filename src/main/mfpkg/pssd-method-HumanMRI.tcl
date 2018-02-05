26efe7e44cc84a1fc326e3cbfe28152876743601
Wilson Liu <wliu5@unimelb.edu.au>
2018-02-05 15:32:10 +1100

    added support for SR modality

--- src/main/mfpkg/pssd-method-HumanMRI.tcl
+++ src/main/mfpkg/pssd-method-HumanMRI.tcl
@@ -13,10 +13,7 @@ set ns mnc
 
 proc createMethod_human_mri { { action 0 } { fillin 0 } } {
 	set name "Standard Human MR"
-	set description "Standard Human MR imaging Method; can be used for any project."
-	set name1 "MRI" 
-	set desc1 "Magnetic resonance acquisition"
-	set type1 "MR"
+	set description "Standard Human MR imaging Method (also supports SR); can be used for any project."
 	#
 	set margs ""
 	# See if Method pre-exists
@@ -46,12 +43,22 @@ proc createMethod_human_mri { { action 0 } { fillin 0 } } {
 	       > \
    	     > \
 	    :step < \
-		    :name ${name1} :description ${desc1} :study < \
-		        :type ${type1} \
+		    :name MRI \
+            :description \"Magnetic resonance acquisition\" \
+            :study < \
+		        :type \"Magnetic Resonance Imaging\" \
 		        :dicom < :modality MR > \
 		        :metadata < :definition -requirement mandatory mnc:scan > \
-		     > \
-	    >"
+		    > \
+	    > \
+        :step < \
+            :name SR \
+            :description \"Structured Report document\" \
+            :study < \
+                :type \"Dose Report\" \
+                :dicom < :modality SR > \
+            > \
+        >"
 	set id2 [xvalue id [om.pssd.method.for.subject.update $args]]
 	if { $id2 == "" } {
 	   # An existng Method was updated
